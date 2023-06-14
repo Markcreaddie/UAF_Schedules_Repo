bd870c7273544c16b3769639f063c239c681e573
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-14 15:28:49 +0300

    Change working directories based on folder reorganization

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import pandas as pd
 import re
 
 
+def select_schedule(item):
+    if item=="Doors":
+        return "door_schedules", "Door No.", "schedules_summary_doors.txt"
+    elif item=="Windows":
+        return "window_schedules", "Window No.", "schedules_summary_windows.txt"
+
+
 def read_df(path, sheet_name):
     return pd.read_excel(path, sheet_name=sheet_name)
 
@@ -19,45 +26,25 @@ def write_to_txt(txt_file,heading,df):
         txt_file.write(f"{heading.strip('Schedules')} \n{df} \n\n")
 
 
-def select_schedule(item):
-    if item=="Doors":
-        return "door_schedules", "Door No.", "schedules_summary_doors.txt"
-    elif item=="Windows":
-        return "window_schedules", "Window No.", "schedules_summary_windows.txt"
-
-
 # navigate to buildings folder
 
-os.chdir("../Buildings")
-cwd = os.path.abspath(os.curdir)
-dir_content = os.listdir()
-building_folders = []
-
-# filter for directories only
-for item in dir_content:
-    if os.path.isdir(os.path.join(cwd, item)):
-        building_folders.append(item)
-
+os.chdir("../REV 01")
+path = os.path.abspath(os.curdir)
+schedules = []
 open("schedules_summary_windows.txt","w").close()
 open("schedules_summary_doors.txt","w").close()
 
-for building in building_folders:
-    path = os.path.join(cwd, building)
-    schedules = []
-    # filter for only files starting with "Schedule" in each directory
-    for file in os.listdir(path):
-        if re.match(r'Schedule*', file):
-            schedules.append(file)
-    # copy contents of files to a pandas dataframe
-    for schedule in schedules:
-        items=['Doors', 'Windows']
-        for item in items:
-            sheet_name,column,txt_file= select_schedule(item)
-            df1= read_df(f'{path}\{schedule}', sheet_name)
-            df2= clean_df(df1,column)
-            write_to_txt(txt_file,schedule,df2)
 
-
-
+for file in os.listdir(path):
+    if re.match(r'Schedule*', file):
+        schedules.append(file)
+# copy contents of files to a pandas dataframe
+for schedule in schedules:
+    items = ['Doors', 'Windows']
+    for item in items:
+        sheet_name, column, txt_file = select_schedule(item)
+        df1 = read_df(f'{path}\{schedule}', sheet_name)
+        df2 = clean_df(df1, column)
+        write_to_txt(txt_file, schedule, df2)
 
 
